cf194486248b069e3055987e8ce96fbfca713862
emanuelssj <emanuelolz@yahoo.com.br>
2019-06-17 10:51:07 -0300

    React-Alerts added

--- UltimaApi_Django_React/UltimaApi/frontendReact/src/components/App.js
+++ UltimaApi_Django_React/UltimaApi/frontendReact/src/components/App.js
@@ -3,19 +3,33 @@ import ReactDOM from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 
+import Alerts from "./layout/Alerts";
+import { Provider as AlertProvider } from 'react-alert';
+import AlertTemplate from 'react-alert-template-basic';
+
 import { Provider } from "react-redux";
 import store from "../store";
+
+// Alert Options
+
+const alertOptions = {
+	timeout: 3000,
+	position: 'top center'
+}
 
 class App extends Component {
 	render() {
 		return (
 			<Provider store={store}>
+			<AlertProvider template={AlertTemplate}{...alertOptions}>
 				<Fragment>
 					<Header />
+					<Alerts/>
 					<div className="container">
 						<Dashboard />
 					</div>
 				</Fragment>
+			</AlertProvider>
 			</Provider>
 		);
 	}
